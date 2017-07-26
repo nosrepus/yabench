93055136f51bbe832f2740f9d71e65205f4ba993
nosrepus <nosrepus@gmail.com>
2017-07-26 12:37:00 -0400

    support static data

--- yabench-oracle/src/main/java/io/github/yabench/oracle/QueryExecutor.java
+++ yabench-oracle/src/main/java/io/github/yabench/oracle/QueryExecutor.java
@@ -6,10 +6,16 @@ import com.hp.hpl.jena.query.QueryExecutionFactory;
 import com.hp.hpl.jena.query.QueryFactory;
 import com.hp.hpl.jena.query.QuerySolution;
 import com.hp.hpl.jena.query.ResultSet;
+import com.hp.hpl.jena.rdf.model.Model;
+import com.hp.hpl.jena.rdf.model.ModelFactory;
 import com.hp.hpl.jena.sparql.engine.binding.Binding;
 import com.hp.hpl.jena.sparql.expr.aggregate.AggregateRegistry;
+import com.hp.hpl.jena.util.FileManager;
 import io.github.yabench.commons.utils.NodeUtils;
 import io.github.yabench.oracle.sparql.AccAvg;
+
+import java.io.InputStream;
+import java.io.StringReader;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
@@ -27,6 +33,37 @@ public class QueryExecutor {
     }
 
     public BindingWindow executeSelect(final TripleWindow input) {
+        String static_file = "/home/l36gao/UWaterloo-WatDiv/bin/Release/1-1/1-1.ttl";
+        Model m = ModelFactory.createDefaultModel();
+        try{
+            InputStream in = FileManager.get().open(static_file);
+            if(in == null){
+                throw new IllegalArgumentException("File" + " not found");
+            }
+            m.read(in, null, "TURTLE");
+        } catch(Exception e){
+            StringReader sr = new StringReader(static_file);
+            try{
+                m.read(sr, null, "RDF/XML");
+            } catch(Exception e1){
+                try{
+                    sr = new StringReader(static_file);
+                    m.read(sr, null, "TURTLE");
+                } catch(Exception e2){
+                    try{
+                        sr = new StringReader(static_file);
+                        m.read(sr, null, "N-TRIPLE");
+                    } catch(Exception e3){
+                        sr = new StringReader(static_file);
+                        m.read(sr, null, "RDF/JSON");
+                    }
+                }
+            }
+            sr.close();
+        }
+
+        input.getModel().add(m);
+
         try (QueryExecution qexec = QueryExecutionFactory.create(query, input.getModel())) {
             ResultSet results = qexec.execSelect();
             final List<Binding> bindings = new ArrayList<>();
