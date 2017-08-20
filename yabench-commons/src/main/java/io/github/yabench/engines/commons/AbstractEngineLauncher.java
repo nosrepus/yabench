5c60ae77ef6b74a481ca2456966002b9ec9283c8
nosrepus <nosrepus@gmail.com>
2017-08-20 16:19:09 -0400

    csparq-static-scale-factor

--- yabench-commons/src/main/java/io/github/yabench/engines/commons/AbstractEngineLauncher.java
+++ yabench-commons/src/main/java/io/github/yabench/engines/commons/AbstractEngineLauncher.java
@@ -75,7 +75,7 @@ public abstract class AbstractEngineLauncher extends AbstractLauncher {
 					long t1 = 0, t2 = 0, time = 0, sleep = 0;
 					logger.info("querystring: "+query.getQueryString());
 					engine.registerQuery(query, serializer);
-					Thread.sleep(80000); //provide setup time for the engine before starting to stream
+					Thread.sleep(60000); //provide setup time for the engine before starting to stream
 					serializer.initialize();
 					logger.info("started sending triples at {}", Instant.now());
 					while ((graph = reader.readNextGraph()) != null) {
